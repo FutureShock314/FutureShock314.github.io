e3b5a6c5fdf351d8d9275fff13f5c2aacc5504f0
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-17 09:28:19 +0100

    test pt3

--- quad-calc/main.js
+++ quad-calc/main.js
@@ -1,8 +1,8 @@
-var term1 = document.getElementById('term-1')
-var term2 = document.getElementById('term-2')
-var term2 = document.getElementById('term-3')
-var title = document.getElementById('title')
+var term1 = document.getElementById('term1')
+var term2 = document.getElementById('term2')
+var term3 = document.getElementById('term3')
+var title = document.querySelector('.title')
 
 term1.addEventListener('input') = () => {
-    title.innerText = term1.textContent
+    title.innerHTML = term1.value
 }
