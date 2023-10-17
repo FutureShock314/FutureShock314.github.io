f7cea351a0bdbd02ba794968252ae0a199ae9f4f
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-17 09:21:02 +0100

    test pt2

--- quad-calc/main.js
+++ quad-calc/main.js
@@ -1,7 +1,8 @@
 var term1 = document.getElementById('term-1')
 var term2 = document.getElementById('term-2')
 var term2 = document.getElementById('term-3')
+var title = document.getElementById('title')
 
-term1.addEventListener(InputEvent) = () => {
-    document.createElement('div').innerText = 'howdy'
+term1.addEventListener('input') = () => {
+    title.innerText = term1.textContent
 }
