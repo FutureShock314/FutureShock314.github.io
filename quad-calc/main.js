05ef055d0d22438f56078f825fc1112e7392cfac
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-17 09:10:55 +0100

    testing js

---
+++ quad-calc/main.js
@@ -0,0 +1,7 @@
+var term1 = document.getElementById('term-1')
+var term2 = document.getElementById('term-2')
+var term2 = document.getElementById('term-3')
+
+term1.addEventListener(InputEvent) = () => {
+    document.createElement('div').innerText = 'howdy'
+}
