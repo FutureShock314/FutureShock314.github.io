f3e5210b6808cdbf7a3fab0a34fae3e74672f245
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-17 15:22:24 +0100

    testing pt9

--- quad-calc/main.js
+++ quad-calc/main.js
@@ -1,6 +1,9 @@
 var term1 = document.getElementById('term1')
+var term1Text = document.getElementById('term1-text')
 var term2 = document.getElementById('term2')
+var term2Text = document.getElementById('term2-text')
 var term3 = document.getElementById('term3')
+var term3Text = document.getElementById('term3-text')
 var title = document.querySelector('.title')
 
 term1.oninput = () => {
@@ -8,10 +11,13 @@ term1.oninput = () => {
 }
 
 term1.onkeydown = () => {
-    title.innerHTML = term1.value
+    term1Text.innerHTML = term1.value
 }
 
 term2.onkeydown = () => {
-    document.createElement(div)
-    document.querySelector('div').innerHTML = term2.value
+    term2Text.innerHTML = term2.value
+}
+
+term3.onkeydown = () => {
+    term3Text.innerHTML = term3.value
 }
