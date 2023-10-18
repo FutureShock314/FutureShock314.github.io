784b2a60243de5a197e2541033c64e60a008092e
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-18 08:19:20 +0000

    codespace deletion check

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var grad = document.querySelector('.grad');
 window.onmousemove = e => {
     var x = e.clientX,
         y = e.clientY;
-        
+
     grad.style.setProperty('--mouse-x', `${x}px`)
     grad.style.setProperty('--mouse-y', `${y}px`)
 }
