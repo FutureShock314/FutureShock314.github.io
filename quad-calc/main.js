b5e5e9ce785a745c825af00aa18e05e83151422c
FutureShock314 <117740939+FutureShock314@users.noreply.github.com>
2023-10-20 08:08:18 +0100

    testing pt11 (js edit)

--- quad-calc/main.js
+++ quad-calc/main.js
@@ -7,13 +7,13 @@ var out_c = document.querySelector('.term3-text')
 var title = document.querySelector('.title')
 
 in_t1.oninput = () => {
-    out_a.innerHTML = in_t1.value
+    out_a.innerHTML = (parseInt(in_t1.value)
 }
 
 in_t2.oninput = () => {
-    out_b.innerHTML = in_t2.value
+    out_b.innerHTML = parseInt(in_t2.value)
 }
 
 in_t3.oninput = () => {
-    out_c.innerHTML = in_t3.value
+    out_c.innerHTML = parseInt(in_t3.value)
 }
